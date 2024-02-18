3c26b32d84b20ab65785bb595540e0f5d44e63fc
josephmcg16 <joseph.mcgovern16@googlemail.com>
2024-02-18 16:26:35 +0000

    Add scrape multiple properties get request

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 from fastapi import FastAPI, HTTPException
 import pandas as pd
+import numpy as np
 
-from nist_scraper import scrape_point
+from nist_scraper import scrape_point, scrape_properties
 from fluid_constants import NistWebbookFluidIDs
 
 
@@ -15,19 +16,45 @@ dummy_db = []
 
 
 @app.get("/fluid-properties/scrape-point/")
-async def root(fluid: str, temperature: float, pressure: float):
+async def get_point(fluid: str, temperature: float, pressure: float):
     """Endpoint for scraping a single point from the NIST Webbook."""
     scraped_data = scrape_point(
         NistWebbookFluidIDs.FLUIDS[fluid], temperature, pressure
     )
     if not isinstance(scraped_data, pd.DataFrame):
         raise HTTPException(status_code=404, detail="No response from NIST Webbook.")
-    response = scraped_data.iloc[0].to_dict()
+    scraped_data["Timestamp"] = pd.Timestamp.now()
+    response = scraped_data.to_dict(orient="records")[0]
+    dummy_db.append(response)
+    return response
+
+
+@app.get("/fluid-properties/scrape-properties/")
+async def get_properties(
+    fluid: str,
+    min_temperature: float,
+    max_temperature: float,
+    min_pressure: float,
+    max_pressure: float,
+    num_points_temperature: int,
+    num_points_pressure: int
+):
+    """Endpoint for scraping a single point from the NIST Webbook."""
+    temperature_range = np.linspace(min_temperature, max_temperature, num_points_temperature)
+    pressure_range = np.linspace(min_pressure, max_pressure, num_points_pressure)
+
+    scraped_data = scrape_properties(
+        NistWebbookFluidIDs.FLUIDS[fluid], temperature_range, pressure_range
+    )
+    if not isinstance(scraped_data, pd.DataFrame):
+        raise HTTPException(status_code=404, detail="No response from NIST Webbook.")
+    scraped_data["Timestamp"] = pd.Timestamp.now()
+    response = scraped_data.to_dict(orient="records")
     dummy_db.append(response)
     return response
 
 
 @app.get("/fluid-properties/get-points/")
-async def get_points():
+async def get_previous_points():
     """Endpoint for getting fluid properties from a dummy database."""
     return dummy_db
--- nist_scraper.py
+++ nist_scraper.py
@@ -1,6 +1,7 @@
 """Module for scraping Thermophysical Properties of Fluid Systems data from the Nist Webbook."""
 from urllib.error import HTTPError
 import pandas as pd
+import numpy as np
 
 
 def scrape_point(
@@ -29,3 +30,73 @@ def scrape_point(
     except HTTPError as exception:
         print(exception)
         return -1
+
+
+def scrape_isotherm(
+    fluid_id: str,
+    isotherm: float,
+    min_pressure: float,
+    max_pressure: float,
+    increment_pressure: float = 0.0,  # Default to scrape the maximum number of points.
+) -> pd.DataFrame:
+    """Scrape fluid properties data from the NIST Webbook for an isotherm.
+
+    Args:
+        fluid_id (str): The fluid ID to scrape.
+        isotherm (float): The process temperature, degC.
+        min_pressure (float): The minimum pressure to scrape, bar(a).
+        max_pressure (float): The maximum pressure to scrape, bar(a).
+        increment_pressure (float): The increments in the pressure range to scrape, bar(a).
+
+    Returns:
+        pd.DataFrame: The scraped fluid data.
+    """
+    scraper_string = (
+        f"https://webbook.nist.gov/cgi/fluid.cgi?T={isotherm}&PLow={min_pressure}&"
+        f"PHigh={max_pressure}&PInc={increment_pressure}&Digits=5&ID={fluid_id}&Action=Load&"
+        "Type=IsoTherm&TUnit=C&PUnit=bar&DUnit=kg%2Fm3&HUnit=kJ%2Fkg&WUnit=m%2Fs&VisUnit=Pa*s&"
+        "STUnit=N%2Fm&RefState=DEF"
+    )
+    try:
+        return pd.read_html(scraper_string)[0]
+    except HTTPError:
+        print("Error: The data could not be scraped from the NIST Webbook.")
+        return -1
+
+
+def scrape_properties(
+    fluid_id: str,
+    temperature_range: np.ndarray,
+    pressure_range: np.ndarray
+) -> pd.DataFrame:
+    """Scrape fluid properties data from the NIST Webbook for a range of temperatures and pressures.
+
+    Args:
+        fluid_id (str): The fluid ID to scrape.
+        temperature_range (np.ndarray): The range of process temperatures, degC.
+        pressure_range (np.ndarray): The range of process pressures, bar(a).
+        show_progress (bool): Whether to show a progress bar.
+
+    Returns:
+        pd.DataFrame: The scraped fluid data.
+    """
+
+    df = pd.DataFrame()
+    for temperature in temperature_range:
+        try:
+            df = pd.concat(
+                [
+                    df,
+                    scrape_isotherm(
+                        fluid_id,
+                        temperature,
+                        pressure_range[0],
+                        pressure_range[-1],
+                        pressure_range[1] - pressure_range[0],
+                    ),
+                ]
+            )
+        except HTTPError:
+            print("Error: The data could not be scraped from the NIST Webbook.")
+            return -1
+    return df
